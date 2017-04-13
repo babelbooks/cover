537988102e2abc24ce6a5ca77286dde28d898313
Le Poney <espeut.clement@gmail.com>
2017-04-13 19:42:36 +0200

    Changed colors and page layout

--- src/client/Components/Home.vue
+++ src/client/Components/Home.vue
@@ -5,8 +5,9 @@
 
       </div>
       <div class="text-vertical-center">
+        <div class="center-vertical">
           <h1><img src="assets/img/logo-bb-title.svg"  alt="Babelbooks" class="svg logo"></h1>
-          <h3>Biblioth&egrave;que coop&eacute;rative</h3>
+          <h3>La biblioth&egrave;que coop&eacute;rative</h3>
           <br>
           <div class="col-md-6 col-md-offset-3">
             <div class="input-group">
@@ -18,15 +19,26 @@
               </span>
             </div>
           </div>
+        </div>
+
+
       </div>
 
       <a class="scroll" href="#block"><span></span>En savoir plus</a>
   </header>
+
   <div class="page-bottom">
     <div class="container block" id="block">
-    <h2>Une bibliothéque pour tout le monde</h2>
+    <h2>Partagez vos livres avec le monde !</h2>
     <div class="col-12 col-md-4 column">
       <span class="glyphicon glyphicon-book"></span>
+      <p>Vos livres prennent la poussière dans votre bibliothéque ? BabelBooks est une librarie partagée et 
+    collaborative où chaque utilisateur mets à disposition ses libres pour tout le monde.
+    Vous pouvez emprunter des livres dans toute la ville et faire de nouvelles découvertes
+    litéraires.</p>
+    </div>
+    <div class="col-12 col-md-4 column">
+    <span class="glyphicon glyphicon-refresh"></span>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
       quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -35,7 +47,30 @@
       proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
     <div class="col-12 col-md-4 column">
-    <span class="glyphicon glyphicon-book"></span>
+    <span class="glyphicon glyphicon-tags"></span>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+  </div>
+
+    <div class="fixed-bg " style="background: url('../assets/img/books.jpeg') no-repeat center center fixed;">
+        <h3 class="heading text-vertical-center">Plus de 87,549 utilisateurs et 20,879,780 livres partagés</h3>
+    </div>
+    <div class="container block" id="block">
+    <h2>Partagez vos livres avec le monde !</h2>
+    <div class="col-12 col-md-4 column">
+      <span class="glyphicon glyphicon-book"></span>
+      <p>Vos livres prennent la poussière dans votre bibliothéque ? BabelBooks est une librarie partagée et 
+    collaborative où chaque utilisateur mets à disposition ses libres pour tout le monde.
+    Vous pouvez emprunter des livres dans toute la ville et faire de nouvelles découvertes
+    litéraires.</p>
+    </div>
+    <div class="col-12 col-md-4 column">
+    <span class="glyphicon glyphicon-refresh"></span>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
       quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -44,7 +79,7 @@
       proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
     <div class="col-12 col-md-4 column">
-    <span class="glyphicon glyphicon-book"></span>
+    <span class="glyphicon glyphicon-tags"></span>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
       quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -52,11 +87,16 @@
       cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
       proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
-
-
   </div>
-  <div class="fixed-bg" style="background: url('../assets/img/books.jpeg') no-repeat center center fixed;"></div>
-  </div>
+
+    <div class="fixed-bg" style="background: url('../assets/img/read.jpg') no-repeat center center fixed;">
+        <h3 class="heading text-vertical-center">Plus de 87,549 utilisateurs et 20,879,780 livres partagés</h3>
+    </div>
+    </div>
+
+    <div class="footer">
+      
+    </div>
 </div>
 
 </template>
@@ -87,6 +127,5 @@ export default {
 </script>
 
 <style lang="sass">
-@import "../assets/css/babelbooks";
 
 </style>
